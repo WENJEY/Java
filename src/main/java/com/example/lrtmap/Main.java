89b3c77acdefd5ee685f3d082d82e50aba174d6a
WENJEY <yunw.j07@gmail.com>
2026-06-30 14:13:36 +0800

    Login

--- src/main/java/com/example/lrtmap/Main.java
+++ src/main/java/com/example/lrtmap/Main.java
@@ -1,7 +1,34 @@
 package com.example.lrtmap;
-
+import java.util.Scanner;
+import java.util.HashMap;
 public class Main {
+
+    static HashMap<String,String> Staff = new HashMap<String , String>();
+    static Scanner s = new Scanner(System.in);
+
+    public static void Login(){
+        Staff.put("S001","wenjey01");
+        System.out.println("Welcome to Rapid KL");
+        System.out.println("-------------------------------------------------");
+
+        System.out.print("Please enter your staff id : ");
+        String id = s.nextLine();
+        System.out.print("Please enter your password : ");
+        String password = s.nextLine();
+
+        if (Staff.containsKey(id)){
+
+            String passwordChecking = Staff.get(id);
+
+            if(password.equals(passwordChecking)){
+                System.out.println("Login Successfully\uD83C\uDF89");
+            }else
+                System.out.println("Invalid Staff ID or password");
+        }else
+            System.out.println("Invalid Staff ID or password");
+
+    }
     public static void main(String[] args) {
-        System.out.println("");
+        Login();
     }
 }
