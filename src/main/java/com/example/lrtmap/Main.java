a12d2776056e959e431527b168a2cb81d610d261
WENJEY <yunw.j07@gmail.com>
2026-07-01 17:02:05 +0800

    Main

--- src/main/java/com/example/lrtmap/Main.java
+++ src/main/java/com/example/lrtmap/Main.java
@@ -3,32 +3,66 @@ import java.util.Scanner;
 import java.util.HashMap;
 public class Main {
 
-    static HashMap<String,String> Staff = new HashMap<String , String>();
     static Scanner s = new Scanner(System.in);
+    static HashMap<String,String> Staff = new HashMap<String , String>();
 
-    public static void Login(){
-        Staff.put("S001","wenjey01");
-        System.out.println("Welcome to Rapid KL");
-        System.out.println("-------------------------------------------------");
+    public static void CreateGraph(){
+        System.out.println("--------------------------------------------");
+        System.out.printf("%20s","Create Graph");
+        System.out.println("--------------------------------------------");
 
-        System.out.print("Please enter your staff id : ");
-        String id = s.nextLine();
-        System.out.print("Please enter your password : ");
-        String password = s.nextLine();
-
-        if (Staff.containsKey(id)){
-
-            String passwordChecking = Staff.get(id);
-
-            if(password.equals(passwordChecking)){
-                System.out.println("Login Successfully\uD83C\uDF89");
-            }else
-                System.out.println("Invalid Staff ID or password");
-        }else
-            System.out.println("Invalid Staff ID or password");
 
     }
+    public static void MainPage (){
+        int answer = -1;
+        while(true) {
+            System.out.printf("\n%35s", "Welcome to the Rapid KL\n");
+            System.out.println("--------------------------------------------");
+            System.out.println("\n    \uD83D\uDE86 Welcome to LRT Navigation System\n");
+            System.out.println("--------------------------------------------");
+            System.out.println("[1] Create Graph");
+            System.out.println("[2] Search for a LRT station");
+            System.out.println("[3] View the LRT Map");
+            System.out.println("[4] Exit");
+            System.out.print("Enter your choice : ");
+
+            if (s.hasNextInt()) {
+                answer = s.nextInt();
+
+                if (answer >= 1 && answer <= 4) {
+                    break;
+                } else {
+                    System.out.println("Invalid selection! Please enter a number from 1 to 4.");
+                }
+            } else {
+                System.out.println("Invalid input! Please enter numbers only.");
+                s.next(); // Remove the invalid input
+            }
+        }
+
+            switch (answer) {
+                case 1:
+                    CreateGraph();
+                    break;
+
+                case 2:
+                    System.out.println("2");
+                    break;
+
+                case 3:
+                    System.out.println("3");
+                    break;
+
+                case 4:
+                    System.out.println("Thank you for using our app!!!");
+                    return;
+
+                default:
+                    System.out.println("Please only enter number between 1 - 4.u");
+
+        }
+    }
     public static void main(String[] args) {
-        Login();
+        MainPage();
     }
 }
